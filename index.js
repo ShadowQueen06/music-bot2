943b36adbf396f3487369f11bab95bddabb88e23
ShadowQueen06 <malk.lol140@gmail.com>
2026-05-31 16:52:27 +0200

    index.js

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 require("dotenv").config();
 
-const {
-  Client,
-  GatewayIntentBits,
-  AttachmentBuilder
-} = require("discord.js");
-
-const {
-  joinVoiceChannel,
-  createAudioPlayer,
-  createAudioResource,
-  AudioPlayerStatus
-} = require("@discordjs/voice");
-
-const play = require("play-dl");
+const { Client, GatewayIntentBits, PermissionsBitField } = require("discord.js");
+const { DisTube } = require("distube");
+const { YtDlpPlugin } = require("@distube/yt-dlp");
 
 const client = new Client({
   intents: [
@@ -25,8 +14,12 @@ const client = new Client({
   ]
 });
 
+const distube = new DisTube(client, {
+  plugins: [new YtDlpPlugin()],
+  emitNewSongOnly: true
+});
+
 const PREFIX = "2P";
-const players = new Map();
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -41,12 +34,13 @@ client.on("guildMemberAdd", member => {
 
 client.on("messageCreate", async message => {
   if (message.author.bot) return;
-   console.log("MESSAGE:", message.content);
+
+  console.log("MESSAGE:", message.content);
 
   const content = message.content.trim();
 
   if (content.startsWith("!setpfp") || content.startsWith("!changepfp")) {
-    if (!message.member.permissions.has("Administrator")) {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
       return message.reply("You need administrator permission.");
     }
 
@@ -80,44 +74,26 @@ client.on("messageCreate", async message => {
   }
 
   try {
-    const searchResult = await play.search(query, {
-      limit: 1
+    await distube.play(voiceChannel, query, {
+      textChannel: message.channel,
+      member: message.member
     });
-
-    if (!searchResult.length) {
-      return message.reply("I couldn't find that song.");
-    }
-
-    const song = searchResult[0];
-    const stream = await play.stream(song.url);
-
-    const connection = joinVoiceChannel({
-      channelId: voiceChannel.id,
-      guildId: message.guild.id,
-      adapterCreator: message.guild.voiceAdapterCreator
-    });
-
-    const player = createAudioPlayer();
-    const resource = createAudioResource(stream.stream, {
-      inputType: stream.type
-    });
-
-    player.play(resource);
-    connection.subscribe(player);
-
-    players.set(message.guild.id, { player, connection });
-
-    message.reply(`Playing: ${song.title}`);
-
-    player.on(AudioPlayerStatus.Idle, () => {
-      connection.destroy();
-      players.delete(message.guild.id);
-    });
-
   } catch (error) {
     console.error(error);
     message.reply("Something went wrong while playing the song.");
   }
 });
+
+distube
+  .on("playSong", (queue, song) => {
+    queue.textChannel.send(`Playing: ${song.name}`);
+  })
+  .on("addSong", (queue, song) => {
+    queue.textChannel.send(`Added: ${song.name}`);
+  })
+  .on("error", (channel, error) => {
+    console.error(error);
+    if (channel) channel.send("Music error. Try another song or link.");
+  });
 
 client.login(process.env.TOKEN);
